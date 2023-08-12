2dabf2d53393634ac66735134343ad0458c1ba92
renatoma951 <renatoma951@gmail.com>
2023-08-12 11:14:48 -0300

    versão inicial do menu

---
+++ executar_instrutor.bat
@@ -0,0 +1,44 @@
+@echo off
+echo.
+chcp 65001 > nul
+echo Digite o número da opção desejada:
+echo.
+echo Opção 1
+echo Opção 2
+echo Opção 3
+echo Opção 4
+
+echo.
+
+set /p opcao=Digite a opção desejada:
+
+echo.
+
+if "%opcao%" == "1" goto op1
+if "%opcao%" == "2" goto op2
+if "%opcao%" == "3" goto op3
+if "%opcao%" == "4" goto op4
+
+:op1 
+echo Opção 1 selecionada. Pressione qualquer tecla para encerrar...
+
+goto fim
+
+:op2 
+echo Opção 2 selecionada. Pressione qualquer tecla para encerrar...
+
+goto fim
+
+:op3
+echo Opção 3 selecionada. Pressione qualquer tecla para encerrar...
+
+goto fim
+
+:op4 
+echo Opção 4 selecionada. Pressione qualquer tecla para encerrar...
+
+goto fim
+
+:fim
+pause > nul
+
