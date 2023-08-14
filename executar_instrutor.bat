ec2fd1b9e01d695c626f6d36e0b67c35598c861a
renatoma951 <renatoma951@gmail.com>
2023-08-14 18:44:04 -0300

    Criação do arquivo aula_1.bat e alteração do código para abrir o arquivo aula_1.bat

--- executar_instrutor.bat
+++ executar_instrutor.bat
@@ -3,7 +3,7 @@ echo.
 chcp 65001 > nul
 echo Digite o número da opção desejada:
 echo.
-echo Opção 1
+echo Opção 1 - Aprender a navegar pelo terminal
 echo Opção 2
 echo Opção 3
 echo Opção 4
@@ -19,26 +19,22 @@ if "%opcao%" == "2" goto op2
 if "%opcao%" == "3" goto op3
 if "%opcao%" == "4" goto op4
 
-:op1 
-echo Opção 1 selecionada. Pressione qualquer tecla para encerrar...
-
+:op1
+echo Opção 1 selecionada. Abrindo aula_1.bat...
+call aulas\aula_1.bat
 goto fim
 
-:op2 
+:op2
 echo Opção 2 selecionada. Pressione qualquer tecla para encerrar...
-
 goto fim
 
 :op3
 echo Opção 3 selecionada. Pressione qualquer tecla para encerrar...
-
 goto fim
 
-:op4 
+:op4
 echo Opção 4 selecionada. Pressione qualquer tecla para encerrar...
-
 goto fim
 
 :fim
 pause > nul
-
